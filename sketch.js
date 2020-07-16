955d8874d2ab361eb0f93c9aa2b312598d24969f
Deepa <deepav2008@gmail.com>
2020-07-16 19:35:51 +0530

    lesson about constraint b/w the bird and the log

--- sketch.js
+++ sketch.js
@@ -1,10 +1,12 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
 var backgroundImg;
+var constraintLog;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -36,6 +38,18 @@ function setup(){
 
     bird = new Bird(100,100);
 
+    constraintLog=new Log(230,180,80,PI/2);
+
+    var options = {
+        bodyA: bird.body,
+        bodyB: constraintLog.body,
+        stiffness: 0.04,
+        length: 10
+    }
+
+    var chain=Constraint.create(options);
+    World.add(world, chain);
+
 }
 
 function draw(){
@@ -61,4 +75,8 @@ function draw(){
 
     bird.display();
     platform.display();
+    constraintLog.display();
+    strokeWeight(3);
+    line(bird.position.x,bird.position.y,constraintLog.positionx,constraintLog.position.y);
+
 }
